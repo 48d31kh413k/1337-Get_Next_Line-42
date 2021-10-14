e8722b3043cc4ea601b0be4436d90810dbeb91d5
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-10-14 11:38:04 +0100

    Update Mandatory Part

--- get_next_line.h
+++ get_next_line.h
@@ -9,6 +9,7 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
 size_t	ft_strlen(char *s);
+char	*new_line(char *line);
 char	*ft_get_line(int fd, char *line);
 char	*ft_get_next_line(char	*line);
 
